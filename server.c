6a7ea6c16de42101a9ddcf4342be30e56eb3c548
guy-harel24 <guy.harel24@gmail.com>
2025-06-01 14:17:27 +0000

    eperimenting more

--- server.c
+++ server.c
@@ -28,8 +28,8 @@ void getargs(int *port, int argc, char *argv[])
 
 int main(int argc, char *argv[])
 {
-    int x;
-    printf("%d\n", x);
+    int y;
+    printf("%d\n", y);
     // Create the global server log
     server_log log = create_log();
 
